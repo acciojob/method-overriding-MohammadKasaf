4fb0590a9d150bd832835a13e6067d99ac037d30
Mohammadkasaf <kaashifchishti611@gmail.com>
2024-04-28 15:31:56 +0530

    commit message

---
+++ src/main/java/com/driver/ClassB.java
@@ -0,0 +1,11 @@
+package com.driver;
+
+public class ClassB extends ClassA {
+
+    @Override
+    public String meth(){
+
+        return "Method is overridden in Extendend class B";
+    }
+
+}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        ClassB obj=new ClassB();
+        System.out.println(obj.meth());
+
+    }
   
 }
