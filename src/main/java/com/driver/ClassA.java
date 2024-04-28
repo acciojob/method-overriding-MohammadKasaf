12187fb019628ddfd7ed5b01b3cbafc6ce69c430
Mohammadkasaf <kaashifchishti611@gmail.com>
2024-04-28 15:43:53 +0530

    commit message

--- src/main/java/com/driver/ClassA.java
+++
@@ -1,9 +0,0 @@
-package com.driver;
-
-public class ClassA {
-
-    public String meth(){
-
-        return "invoking method from classvA";
-    }
-}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ClassB obj=new ClassB();
-        System.out.println(obj.meth());
+
 
     }
   
