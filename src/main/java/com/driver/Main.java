637b215ac9e3234911ffabb4135e50e285f042b9
Mohammadkasaf <kaashifchishti611@gmail.com>
2024-05-04 00:56:56 +0530

    commit message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,11 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         B obj=new B();
-        //obj.meth();
         //System.out.println(obj.meth());
 
         //after performing 4th step
-        obj.meth();
         System.out.println(obj.meth());
 
     }
